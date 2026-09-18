beca0430d866a7a48e3dbe98dd96ce00e8fe42bb
hahajax <unoreverse632@gmail.com>
2026-09-17 19:09:51 -0600

    cleanup ProtectionChangeClient.java

--- src/main/java/com/hahajax/protectionchange/ProtectionChangeClient.java
+++ src/main/java/com/hahajax/protectionchange/ProtectionChangeClient.java
@@ -1,12 +1,9 @@
 package com.hahajax.protectionchange;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
@@ -20,12 +17,5 @@ public class ProtectionChangeClient {
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-    }
-
-    @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        ProtectionChange.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ProtectionChange.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
